f7d9c821aa9acca7c0052495e62313214938d934
jamilly-campelo <97696411+jamilly-campelo@users.noreply.github.com>
2022-10-06 12:02:52 -0300

    add arquivos atualizados

---
+++ partida.php
@@ -0,0 +1,46 @@
+<?php
+    $op = $_GET['operacao']; // verbos são como arrays do Java
+    $n1 = rand(1, 99);
+    $n2 = rand(1, 99);
+
+    switch ($op) {
+        case 'soma':
+            $op = '+';
+            break;
+        case 'subtracao':
+            $op = '-';
+            break;
+        case 'multiplicacao':
+            $op = 'x';
+            break;
+        default:
+            $op = '/';
+    }
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculus - Uma forma divertida de cálculo mental!</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <h1 id="titulo">Nova Partida!</h1>
+    <div class="partida">
+        <div class="numeros">
+            <span><?php echo $n1; ?></span>
+            <span><?php echo $op; ?></span>
+            <span><?php echo $n2; ?></span>
+        </div>
+        <div class="resultado">
+            <form action="/resultado.php?n1=<?=$n1?>&n2=<?=$n2?>&<?=$op?>"> <!-- para onde o formuário será enviado -->
+                <legend>Resposta</legend>
+                <input class="form" type="number">
+                <button class="btn" type="submit">Submeter</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ resultado.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculus - Uma forma divertida de cálculo mental!</title>
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+    <div class="resultado">
+        <img src="https://i0.wp.com/abes-dn.org.br/wp-content/uploads/2022/05/quality-g096c0fb43_1920.png?fit=1200%2C1200&ssl=1" alt="deu certo!">
+        <h1>Parabéns, Você acertou!</h1>
+        <a href="#">Tentar Novamente</a>
+    </div>
+    <div class="resultado">
+        <img src="https://imagensemoldes.com.br/wp-content/uploads/2020/08/Imagem-X-Vermelho-PNG-1015x1024.png" alt="deu ruim!">
+        <h1>Que pena, Você errou!</h1>
+        <a href="#">Tentar Novamente</a>
+    </div>
+</body>
+</html>
