b15ef705845ff8098270e9f3f55f379d23504e0b
jamilly-campelo <97696411+jamilly-campelo@users.noreply.github.com>
2022-11-17 11:54:28 -0300

    Add files via upload

--- calculadora/app/Http/Controllers/OperacaoController.php
+++ calculadora/app/Http/Controllers/OperacaoController.php
@@ -7,19 +7,23 @@ use Illuminate\Http\Request; //import do java
 class OperacaoController extends Controller {
     
     public function somar($n1, $n2) {
-        return $n1 + $n2;
+        $resultado = $n1 + $n2;
+        return view('resultado', compact('resultado'));
     }
 
     public function subtrair($n1, $n2) {
-        return $n1 - $n2;
+        $resultado = $n1 - $n2;
+        return view('resultado', compact('resultado'));
     }
 
     public function multiplicar($n1, $n2) {
-        return $n1 * $n2;
+        $resultado = $n1 * $n2;
+        return view('resultado', compact('resultado'));
     }
 
     public function dividir($n1, $n2) {
-        return $n1 / $n2;
+        $resultado = $n1 / $n2;
+        return view('resultado', compact('resultado'));
     }
 
 }
